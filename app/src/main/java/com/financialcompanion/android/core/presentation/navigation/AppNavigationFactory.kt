f0d98f4f268befb07f13a2243eaa77cdffff167f
Jamal <aliev.djamal.2000@gmail.com>
2023-12-14 22:44:55 +0300

    updated core

---
+++ app/src/main/java/com/financialcompanion/android/core/presentation/navigation/AppNavigationFactory.kt
@@ -0,0 +1,9 @@
+package com.financialcompanion.android.core.presentation.navigation
+
+import com.jamal_aliev.navigationcontroller.core.NavigationControllerFactory
+
+class AppNavigationFactory : NavigationControllerFactory() {
+    init {
+
+    }
+}
---
+++ app/src/main/java/com/financialcompanion/android/core/presentation/navigation/AppScreen.kt
@@ -0,0 +1,8 @@
+package com.financialcompanion.android.core.presentation.navigation
+
+import me.aartikov.alligator.Screen
+import java.io.Serializable
+
+sealed class AppScreen : Screen, Serializable {
+
+}
