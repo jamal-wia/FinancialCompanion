6faf075b390c538ec7ba6ac69630ea981b9dc701
Murad <muradkartal91@gmail.com>
2023-12-17 21:48:35 +0300

    greetings - create new fragment

---
+++ app/src/main/java/com/financialcompanion/android/app/greetings/presentation/GreetingsFragment.kt
@@ -0,0 +1,7 @@
+package com.financialcompanion.android.app.greetings.presentation
+
+import com.financialcompanion.android.core.presentation.base.BaseFragment
+
+class GreetingsFragment : BaseFragment() {
+
+}
