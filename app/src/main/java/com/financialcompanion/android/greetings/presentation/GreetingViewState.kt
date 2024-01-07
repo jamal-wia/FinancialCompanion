715104085cfed3a4a676e98467ef086886b7ead1
Murad <muradkartal91@gmail.com>
2024-01-07 22:52:34 +0300

    "Greeting" - added new string resources

--- app/src/main/java/com/financialcompanion/android/greetings/presentation/GreetingViewState.kt
+++ app/src/main/java/com/financialcompanion/android/greetings/presentation/GreetingViewState.kt
@@ -12,24 +12,29 @@ sealed class GreetingViewState {
                 image = R.drawable.first_screen
             ),
             GreetingUiModel(
-                titleId = R.string.title_screen_first,
-                descriptionId = R.string.description_screen_first,
-                image = R.drawable.first_screen
+                titleId = R.string.title_screen_second,
+                descriptionId = R.string.description_screen_second,
+                image = R.drawable.second_screen
             ),
             GreetingUiModel(
-                titleId = R.string.title_screen_first,
-                descriptionId = R.string.description_screen_first,
-                image = R.drawable.first_screen
+                titleId = R.string.title_screen_third,
+                descriptionId = R.string.description_screen_third,
+                image = R.drawable.third_screen
             ),
             GreetingUiModel(
-                titleId = R.string.title_screen_first,
-                descriptionId = R.string.description_screen_first,
-                image = R.drawable.first_screen
+                titleId = R.string.title_screen_fourth,
+                descriptionId = R.string.description_screen_fourth,
+                image = R.drawable.fourth_screen
             ),
             GreetingUiModel(
-                titleId = R.string.title_screen_first,
-                descriptionId = R.string.description_screen_first,
-                image = R.drawable.first_screen
+                titleId = R.string.title_screen_fifth,
+                descriptionId = R.string.description_screen_fifth,
+                image = R.drawable.fifth_screen
+            ),
+            GreetingUiModel(
+                titleId = R.string.title_screen_sixth,
+                descriptionId = R.string.description_screen_sixth,
+                image = R.drawable.sixth_screen
             )
         )
     }
