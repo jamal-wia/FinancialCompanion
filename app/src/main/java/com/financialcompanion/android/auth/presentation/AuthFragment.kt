b6960dc4ffe79a47e7724cfface283a593cd2fc1
Murad <muradkartal91@gmail.com>
2024-01-10 21:16:58 +0300

    "Auth" - Created auth fragment and auth view model

---
+++ app/src/main/java/com/financialcompanion/android/auth/presentation/AuthFragment.kt
@@ -0,0 +1,6 @@
+package com.financialcompanion.android.auth.presentation
+
+import com.financialcompanion.android.core.presentation.base.BaseFragment
+
+class AuthFragment : BaseFragment() {
+}
---
+++ app/src/main/java/com/financialcompanion/android/auth/presentation/AuthViewModel.kt
@@ -0,0 +1,6 @@
+package com.financialcompanion.android.auth.presentation
+
+import com.financialcompanion.android.core.presentation.mvvm.BaseViewModel
+
+class AuthViewModel : BaseViewModel() {
+}
